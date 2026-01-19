635bd395d876141033bee60152fb9ea15bc46ccd
tak4da <japonez1337@gmail.com>
2026-01-19 08:22:30 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,10 +36,10 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data_simple.db")
 # ID чата-конфы магазина (Бализаж), куда слать уведомления
 BALIZAG_CHAT_ID = -1002815036494      # правильный ID группы
 # ID ветки в Бализаж (если нужна). Пока None — можно потом подставить.
-BALIZAG_THREAD_ID = 445
+BALIZAG_THREAD_ID = 29984
 
 # ID админов, которые могут подтверждать/возвращать замечания
-ADMIN_IDS = {377226664, 1705170078}
+ADMIN_IDS = {208004861}
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
