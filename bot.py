70cc5e84c8bf3781e3b73a00f600f05b58b094e2
tak4da <japonez1337@gmail.com>
2026-01-21 19:17:24 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -864,7 +864,7 @@ async def finish_inspection(message: types.Message):
                 f"👷 Аудитор: {inspector_name}\n"
                 f"📅 Дата аудита: {ins_date.strftime('%d.%m.%Y')}\n"
                 f"📍 Исправить до: {control_date.strftime('%d.%m.%Y')}\n"
-                f"🤖 Перейти в бота: @BalisageAudit_bot"
+                f"🤖 Перейти в бота: @BalisageAudit_80_bot"
             )
 
             await bot.send_message(
